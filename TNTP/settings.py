a453cb9a428ce26f5070ee7b9c2a12aa2fe39f55
TQ_09Pro <89725047+nguyenphuoc2k9@users.noreply.github.com>
2023-07-22 17:13:31 +0700

    Update settings.py

--- TNTP/settings.py
+++ TNTP/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 APPEND_SLASH = False
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
-STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
